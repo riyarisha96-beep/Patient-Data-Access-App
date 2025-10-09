274870656c0e6967b741da8c941b34a672e1919e
Shreya Singh <shreyasingh@Shreyas-MacBook-Air.local>
2025-10-09 22:36:43 +1200

    Patient Data Access DApp (Hardhat + Streamlit) – Sepolia ready

--- blockchain/hardhat.config.js
+++ blockchain/hardhat.config.js
@@ -1,10 +1,16 @@
 require("@nomicfoundation/hardhat-toolbox");
 require("dotenv").config();
 
-module.exports = {
-  solidity: "0.8.24",
-  networks: {
-    hardhat: {},
-    localhost: { url: "http://127.0.0.1:8545" }
-  }
+const networks = {
+  hardhat: {},
+  localhost: { url: "http://127.0.0.1:8545" },
 };
+
+if (process.env.SEPOLIA_RPC && process.env.DEPLOYER_PRIVATE_KEY) {
+  networks.sepolia = {
+    url: process.env.SEPOLIA_RPC,
+    accounts: [process.env.DEPLOYER_PRIVATE_KEY],
+  };
+}
+
+module.exports = { solidity: "0.8.24", networks };
